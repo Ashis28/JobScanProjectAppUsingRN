b6f1eba44aefa52d6b5800f71579b92bc05f5b73
Ashis28 <dashashiskumar28@example.com>
2025-08-08 01:35:36 +0530

    upto api data fetching is done

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -3,10 +3,36 @@ import { View, Text ,TouchableOpacity, FlatList, ActivityIndicator } from 'react
 import { useRouter } from 'expo-router'
 import styles from './popularjobs.style'
 import { COLORS , SIZES } from '../../../constants'
+import PopularJobsCard from '../../common/cards/popular/PopularJobCard'
+import { isLoading } from 'expo-font'
 const Popularjobs = () => {
+
+  const route = useRouter();
+  const isLoading = false;
+  const error = false;
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}> 
+        <Text style={styles.headerTitle}>Popularjobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.cardsContainer}>
+          {isLoading?(<ActivityIndicator/>):error?
+          (<Text>Something went wrong</Text>):
+           (<FlatList
+            data={[1,2,3,4,5]}
+            renderItem={({item}) => (
+              <PopularJobsCard item={item}/>
+            )}
+            keyExtractor={item=>item?.job_id}
+            contentContainerStyle={{columnGap:SIZES.medium}}
+            horizontal
+            />)}
+      </View>
     </View>
   )
 }

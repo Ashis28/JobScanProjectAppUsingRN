0625008ffb655cb2b7f83d263cced6a4e47b37e2
Ashis28 <dashashiskumar28@example.com>
2025-08-08 04:11:01 +0530

    nearby jobs will create after

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, Text,TouchableOpacity, Image} from 'react-native'
-
+import {checkImageURL} from '../../../../utils'
 import styles from './popularjobcard.style'
 
 const PopularJobCard = ({item,selectedJob,handleCardPress}) => {
@@ -8,8 +8,18 @@ const PopularJobCard = ({item,selectedJob,handleCardPress}) => {
     <TouchableOpacity style={styles.container(selectedJob, item)} onPress={() => handleCardPress(item)}>
       <TouchableOpacity style={styles.logoContainer(selectedJob,item)}>
         <Image 
-        source={{uri:item.employer_logo}}/>
+        source={{uri:item.employer_logo}}
+        resizeMode='contain'
+        style={styles.logoImage}/>
       </TouchableOpacity>
+      <Text style={styles.companyName } numberOfLines={1}>{item.employer_name}</Text>
+
+      <View style={styles.infoContainer}>
+          <View style={styles.jobContainer}>
+              <Text numberOfLines={1}>{item.job_title}</Text>
+              <Text style={styles.location}>{item.job_country}</Text>
+          </View>
+      </View>
     </TouchableOpacity>
   )
 }
--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -38,7 +38,7 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1,2,3,4,5]}
+            data={data}
             renderItem={({ item }) => (
               <PopularJobsCard item={item} />
             )}

051cac5ab54ae21a1efb4535ee815d56c5f5cc14
Ashis28 <dashashiskumar28@example.com>
2025-08-09 10:20:04 +0530

    THis is just to go back to its prev commits

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,27 +1,38 @@
-import React from 'react'
-import { View, Text,TouchableOpacity, Image} from 'react-native'
-import {checkImageURL} from '../../../../utils'
-import styles from './popularjobcard.style'
+import React from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { checkImageURL } from '../../../../utils';
+import styles from './popularjobcard.style';
 
-const PopularJobCard = ({item,selectedJob,handleCardPress}) => {
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    <TouchableOpacity style={styles.container(selectedJob, item)} onPress={() => handleCardPress(item)}>
-      <TouchableOpacity style={styles.logoContainer(selectedJob,item)}>
-        <Image 
-        source={{uri:item.employer_logo}}
-        resizeMode='contain'
-        style={styles.logoImage}/>
-      </TouchableOpacity>
-      <Text style={styles.companyName } numberOfLines={1}>{item.employer_name}</Text>
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <View style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={{
+            uri: checkImageURL(item.employer_logo)
+              ? item.employer_logo
+              : 'https://dummyimage.com/100x100/cccccc/000000&text=No+Logo',
+          }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </View>
+
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item.employer_name}
+      </Text>
 
       <View style={styles.infoContainer}>
-          <View style={styles.jobContainer}>
-              <Text numberOfLines={1}>{item.job_title}</Text>
-              <Text style={styles.location}>{item.job_country}</Text>
-          </View>
+        <View style={styles.jobContainer}>
+          <Text numberOfLines={1}>{item.job_title}</Text>
+          <Text style={styles.location}>{item.job_country}</Text>
+        </View>
       </View>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-export default PopularJobCard
+export default PopularJobCard;
